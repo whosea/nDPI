384a5fd97835adc76989149344ca5b333281f3b0
Vitaly Lavrov <vel21ripn@gmail.com>
2020-07-15 20:27:17 +0300

    Fix memory leak in ndpi_init_protocol_match()

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -506,34 +506,18 @@ static int ndpi_string_to_automa(struct ndpi_detection_module_struct *ndpi_str, 
 
 /* ****************************************************** */
 
-static int ndpi_add_host_url_subprotocol(struct ndpi_detection_module_struct *ndpi_str, char *_value, int protocol_id,
+static int ndpi_add_host_url_subprotocol(struct ndpi_detection_module_struct *ndpi_str, char *value, int protocol_id,
                                          ndpi_protocol_category_t category, ndpi_protocol_breed_t breed) {
-  int rv;
-  char *value = ndpi_strdup(_value);
 
-  if(!value)
-    return(-1);
+  int rv = ndpi_string_to_automa(ndpi_str, &ndpi_str->host_automa, value, protocol_id, category, breed, 1);
 
 #ifdef DEBUG
-  NDPI_LOG_DBG2(ndpi_str, "[NDPI] Adding [%s][%d]\n", value, protocol_id);
+  NDPI_LOG_DBG2(ndpi_str, "[NDPI] Adding [%s][%d] %s\n", value, protocol_id,
+		  rv ? "Error":"OK");
 #endif
-
-  rv = ndpi_string_to_automa(ndpi_str, &ndpi_str->host_automa, value, protocol_id, category, breed, 1);
-
-  if(rv != 0)
-    ndpi_free(value);
 
   return(rv);
 }
-
-/* ****************************************************** */
-
-#ifdef CODE_UNUSED
-int ndpi_add_content_subprotocol(struct ndpi_detection_module_struct *ndpi_str, char *value, int protocol_id,
-                                 ndpi_protocol_category_t category, ndpi_protocol_breed_t breed) {
-  return(ndpi_string_to_automa(ndpi_str, &ndpi_str->content_automa, value, protocol_id, category, breed, 0));
-}
-#endif
 
 /* ****************************************************** */
 
