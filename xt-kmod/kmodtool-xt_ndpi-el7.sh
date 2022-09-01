da394a0c4f422a74938094f6081f3ef6006feb54
ht <whosea@yeah.net>
2022-09-01 20:26:33 +0800

    no message

--- xt-kmod/kmodtool-xt_ndpi-el7.sh
+++ xt-kmod/kmodtool-xt_ndpi-el7.sh
@@ -244,9 +244,9 @@ echo "%files         -n kmod-${kmod_name}${dashvariant}"
 if [ "" == "$override_filelist" ];
 then
     echo "%defattr(644,root,root,755)"
-    echo "/lib/modules/${verrel}${dotvariant}/"
-    echo "/usr/lib64/xtables/libxt_NDPI.so"
-    echo "/usr/lib64/xtables/libxt_ndpi.so" 
+    echo "%files /lib/modules/${verrel}${dotvariant}/"
+    echo "%files /usr/lib64/xtables/libxt_NDPI.so"
+    echo "%files /usr/lib64/xtables/libxt_ndpi.so"
     echo "%config /etc/depmod.d/kmod-${kmod_name}.conf"
     echo "%doc /usr/share/doc/kmod-${kmod_name}-%{version}/"
 else
